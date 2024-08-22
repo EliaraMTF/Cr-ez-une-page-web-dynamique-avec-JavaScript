5aaa5947d2f1ba3f101349ceb5c31fd18df1e64a
Lucas Bussereau <lucas.bussereau1202@outlook.fr>
2024-08-22 18:00:10 +0200

    Changement

--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -1,5 +1,17 @@
 //appeler la fonction qui récupère les données de l'api 
-
+fetch("http://localhost:5678/api-docs/") //Envoie une requête GET à l'URL spécifiée.
+    .then(response => {                  //Traite la réponse obtenue.
+        if (!response.ok) {              //Vérifie si la réponse est correcte
+            throw new Error(`Erreur HTTP! Statut: ${response.status}`); //Si la réponse est incorrecte = erreur
+        }
+        return response.json();
+    })
+    .then(data => {                      //Les données JSON sont disponibles ici.
+        console.log(data);               //Affiche les données dans la console (ou les traite selon vos besoins).
+    })
+    .catch(error => {                    //Gère toute erreur qui se produit lors de la requête.
+        console.error('Une erreur est survenue:', error);
+    });
 //créer la fonction pour récuperer les données de l'api
     
 //Une fois les donner récuperés, appeler la fonction qui me permet d'injecter les données dans l'html 
