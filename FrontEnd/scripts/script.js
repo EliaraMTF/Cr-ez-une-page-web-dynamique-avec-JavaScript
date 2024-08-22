231bb6ef1ac699b67d3defedb1f5588ebf495b3d
Lucas Bussereau <lucas.bussereau1202@outlook.fr>
2024-08-22 16:48:49 +0200

    Début du projet

---
+++ FrontEnd/scripts/script.js
@@ -0,0 +1,11 @@
+//appeler la fonction qui récupère les données de l'api 
+
+//créer la fonction pour récuperer les données de l'api
+    
+//Une fois les donner récuperés, appeler la fonction qui me permet d'injecter les données dans l'html 
+
+    //créer la fonction pour injecter les données dans l'html
+
+//récuperer les catégories à partir de l'api
+
+//mettre en place les gestionnaire d'evenement pour le clic sur les bouton des catégories
