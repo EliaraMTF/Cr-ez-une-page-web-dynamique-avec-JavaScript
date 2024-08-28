5c8251f4b08ff3fdccd5860619df1c572fe93188
Lucas Bussereau <lucas.bussereau1202@outlook.fr>
2024-08-28 20:16:08 +0200

    Changement script

--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -10,7 +10,7 @@ function fetchData(){
     })
     .then(data => {                      //Les données JSON sont disponibles ici.
         console.log(data);               //Affiche les données dans la console (ou les traite selon vos besoins).
-        
+        injectDataIntoHTML(data);
     })
     .catch(error => {                    //Gère toute erreur qui se produit lors de la requête.
         console.error('Une erreur est survenue:', error);
@@ -18,23 +18,26 @@ function fetchData(){
 }
 //Une fois les donner récuperés, appeler la fonction qui me permet d'injecter les données dans l'html 
 
-    //créer la fonction pour injecter les données dans l'html
-    function injectDataIntoHTML(data) {
-        const gallery = document.querySelector('gallery'); // Sélectionne la galerie
-        gallery.innerHTML = ''; // Réinitialise le contenu de la galerie
-    
-        data.forEach(work => {
-            const figure = document.createElement('figure'); // Crée un élément figure
-            figure.innerHTML = `
-                <img src="${work.imageUrl}" alt="${work.title}">
-                <figcaption>${work.title}</figcaption>
-            `;
-            gallery.appendChild(figure); // Ajoute la figure à la galerie
-        });
-    }
+//créer la fonction pour injecter les données dans l'html
+function injectDataIntoHTML(data) {
+    const galleryHTML = document.getElementById('gallery'); // Sélectionne la galerie
+
+    data.forEach(work => {
+        const figureHTML = document.createElement('figure'); // Crée un élément figure
+        const imgHTML  = document.createElement('img');
+        const figcaptionHTML = document.createElement('figcaption');
+
+        imgHTML.src = work.imageUrl;
+        imgHTML.alt = work.title;
+        figcaptionHTML.innerHTML = work.title;
+        figureHTML.appendChild(imgHTML);
+        figureHTML.appendChild(figcaptionHTML);
+        galleryHTML.appendChild(figureHTML);
+    });
+}
 
 //récuperer les catégories à partir de l'api
-function fetchCategories() {
+/*function fetchCategories() {
     console.log("fetchCategories");
     fetch("http://localhost:5678/api/categories") //Envoie une requête GET à l'URL spécifiée.
         .then(response => { //Traite la réponse obtenue.
@@ -49,7 +52,7 @@ function fetchCategories() {
         .catch(error => {
             console.error('Une erreur est survenue lors de la récupération des catégories:', error);
         });
-}
+}*/
 //mettre en place les gestionnaire d'evenement pour le clic sur les bouton des catégories
 
 //appeler la fonction qui récupère les données de l'api 
